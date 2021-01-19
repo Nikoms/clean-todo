5643b3df848cdcb6d9de3deab5349e86be5b7aa4
Nicolas De Boose <nikoms@gmail.com>
2021-01-20 00:35:21 +0100

    Better naming

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,24 +1,24 @@
 import {useState} from 'react';
 
-function createData(title, done) {
+function createTodo(title, done) {
   return {title, done};
 }
 
 const TodoList = () => {
   const [todos, setTodoList] = useState(() => [
-    createData('Frozen yoghurt', false),
-    createData('Ice cream sandwich', false),
-    createData('Eclair', false),
-    createData('Cupcake', false),
-    createData('Gingerbread', false),
+    createTodo('Frozen yoghurt', false),
+    createTodo('Ice cream sandwich', false),
+    createTodo('Eclair', false),
+    createTodo('Cupcake', false),
+    createTodo('Gingerbread', false),
   ]);
 
-  const addTodos = () => setTodoList([createData('Relax! Edition will come...', false), ...todos]);
+  const addEmptyTodo = () => setTodoList([createTodo('Relax! Edition will come...', false), ...todos]);
 
   return <>
     <table>
       <thead>
-      <td rowSpan={2}>My todos <button onClick={addTodos}>Add</button></td>
+      <td rowSpan={2}>My todos <button onClick={addEmptyTodo}>Add</button></td>
       </thead>
       <tbody>
       {todos.map((row) => (
