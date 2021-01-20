c4f1f7b1af56609e1042d0504fa0fa304ebb2551
Nicolas De Boose <nikoms@gmail.com>
2021-01-20 16:18:13 +0100

    key is of course needed in map

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,7 +1,8 @@
 import {useState} from 'react';
+import {v4} from 'uuid';
 
 function createTodo(title, done) {
-  return {title, done};
+  return {id: v4(), title, done};
 }
 
 const TodoList = () => {
@@ -18,13 +19,15 @@ const TodoList = () => {
   return <>
     <table>
       <thead>
-      <td rowSpan={2}>My todos <button onClick={addEmptyTodo}>Add</button></td>
+      <tr>
+        <th rowSpan={2} align="left">My todos <button onClick={addEmptyTodo}>Add</button></th>
+      </tr>
       </thead>
       <tbody>
-      {todos.map((row) => (
-        <tr>
-          <td>{row.title}</td>
-          <td><input type="checkbox" value="1" checked={row.done} disabled={true}/></td>
+      {todos.map((todo) => (
+        <tr key={todo.id}>
+          <td>{todo.title}</td>
+          <td><input type="checkbox" value="1" checked={todo.done} disabled={true}/></td>
         </tr>
       ))}
       </tbody>
