1cca0cfa272fbf2d39b8ae9fffe925876c859c1c
Nicolas De Boose <nikoms@gmail.com>
2021-01-22 10:20:08 +0100

    Doing computation in a "useMemo" can solve performance issue

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useMemo, useState} from 'react';
 import {v4} from 'uuid';
 
 function createTodo(title, done) {
@@ -22,15 +22,15 @@ const TodoList = () => {
     fetchTodosFromApi().then(list => setTodoList(list));
   }, []);
 
-  //2. We still have our counters that are recalculate on every render
-  const ongoingCount = todos.filter(t => t.done).length;
-  const doneCount = todos.filter(t => !t.done).length;
+  //2. useMemo's callback will only be called when the dependency (todos) changes 
+  const ongoingCount = useMemo(() => todos.filter(t => t.done).length, [todos]);
+  const doneCount = useMemo(() => todos.filter(t => !t.done).length, [todos]);
 
-  // 3. And we have a new cool feature that count the number of time we click on titles (best feature ever asked by the CEO)
   const [clickCount, setClickCount] = useState(0);
 
-  //Conclusion: 
-  // - Calling "setClickCount" will re-render the component and our "ongoingCount" and "doneCount" will be recalculate everytime... It's fine for this example, but it can be problematic if the computation is heavy and can block your main thread
+  // 2 conclusions: 
+  // - Avoid using useEffect "in cascade". If you only use "useEffect" to set a value to a local state and nobody else uses the setState, then you probably should consider "useMemo" instead
+  // - Use "useMemo" for heavy computation or when you have a lot of re-rendering
 
   const addEmptyTodo = () => setTodoList([createTodo('Relax! Edition will come...', false), ...todos]);
   const markAsDone = (index) => setTodoList([...todos.slice(0, index), {
