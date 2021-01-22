a0a4d5cbd4f5584091ee7e20367641f026fddee5
Nicolas De Boose <nikoms@gmail.com>
2021-01-22 09:25:01 +0100

    Doing the same computation on every re-render can be a problem
    # Conflicts:
    #	src/Todo/TodoList.js

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -18,13 +18,19 @@ const fetchTodosFromApi = async () => {
 const TodoList = () => {
   const [todos, setTodoList] = useState([]);
   useEffect(function callApi() {
+    //1. Imagine that we are calling an api to get todos 
     fetchTodosFromApi().then(list => setTodoList(list));
   }, []);
 
+  //2. We still have our counters that are recalculate on every render
   const ongoingCount = todos.filter(t => t.done).length;
   const doneCount = todos.filter(t => !t.done).length;
 
-  console.log('render with', {total: todos.length, ongoingCount, doneCount});
+  // 3. And we have a new cool feature that count the number of time we click on titles (best feature ever asked by the CEO)
+  const [clickCount, setClickCount] = useState(0);
+
+  //Conclusion: 
+  // - Calling "setClickCount" will re-render the component and our "ongoingCount" and "doneCount" will be recalculate everytime... It's fine for this example, but it can be problematic if the computation is heavy and can block your main thread
 
   const addEmptyTodo = () => setTodoList([createTodo('Relax! Edition will come...', false), ...todos]);
   const markAsDone = (index) => setTodoList([...todos.slice(0, index), {
@@ -37,7 +43,7 @@ const TodoList = () => {
     <table>
       <thead>
       <tr>
-        <th rowSpan={2} align="left">My todos ({ongoingCount} ongoing /{doneCount} done)
+        <th rowSpan={2} align="left">My todos ({ongoingCount} ongoing /{doneCount} done/ {clickCount} clicks)
           <button onClick={addEmptyTodo}>Add</button>
         </th>
       </tr>
@@ -45,7 +51,7 @@ const TodoList = () => {
       <tbody>
       {todos.map((todo, index) => (
         <tr key={todo.id}>
-          <td>{todo.title}</td>
+          <td onClick={() => setClickCount(() => clickCount + 1)}>{todo.title}</td>
           <td><input type="checkbox" value="1" checked={todo.done} onChange={() => markAsDone(index)}/></td>
         </tr>
       ))}
