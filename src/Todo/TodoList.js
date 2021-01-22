a6d8b88db9adff414f275546e2102662df0356cb
Nicolas De Boose <nikoms@gmail.com>
2021-01-22 08:51:05 +0100

    What happens when useEffect contains a async method with multiple setState?

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -26,26 +26,27 @@ const TodoList = () => {
   const [ongoingCount, setOngoingCount] = useState(0);
   const [doneCount, setDoneCount] = useState(0);
 
-  // 3. We will update the counters when the list changes
+  // 3. But what if the setter are in a async method ?
   useEffect(function refreshCounters() {
-    setDoneCount(todos.filter(t => t.done).length);
-    setOngoingCount(todos.filter(t => !t.done).length);
+    (async () => {
+      new Promise(resolve => setTimeout(() => {
+        setDoneCount(todos.filter(t => t.done).length);
+        setOngoingCount(todos.filter(t => !t.done).length);
+        resolve();
+      }, 1));
+    })();
   }, [todos]);
 
   //4. See how many render ?
   console.log('render with', {total: todos.length, ongoingCount, doneCount});
-  // On first load it renders 3 times:
-  // render with { total: 0, ongoingCount: 0, doneCount: 0 } ==> Before "callApi"
-  // render with { total: 5, ongoingCount: 0, doneCount: 0 } ==> After "callApi"
-  // render with { total: 5, ongoingCount: 5, doneCount: 0 } ==> After "refreshCounters". React is smart an only re-render when the value really changes. Here "doneCount" is "updated" from 0 to 0, so it does not re-render
-
-  // When I set one of the todos as "done", it re-renders only 2 times (but 3 things changed: todos, ongoingCount and doneCount, do you know why?)
+  // When I set one of the todos as "done", it re-renders 3 times
   // render with { total: 5, ongoingCount: 5, doneCount: 0 } ==> when setTodoList is called
-  // render with { total: 5, ongoingCount: 4, doneCount: 1 } ==> when setDoneCount/setOngoingCount is called (after useEffect)
-  
+  // render with { total: 5, ongoingCount: 5, doneCount: 1 } ==> when setDoneCount is called
+  // render with { total: 5, ongoingCount: 4, doneCount: 1 } ==> when setOngoingCount is called
+
   //2 conclusions: 
-  // - We have too many render!
-  // - React seems to wait the end of the useEffect to re-render the component (smart!)
+  // - We still have too many render!
+  // - Once you are outside the "useEffect" thread, you have as many render as "setter"
 
   const addEmptyTodo = () => setTodoList([createTodo('Relax! Edition will come...', false), ...todos]);
   const markAsDone = (index) => setTodoList([...todos.slice(0, index), {
