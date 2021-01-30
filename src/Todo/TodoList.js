98fb4046dc5ab5d819b2df34c232c02c522453f4
Nicolas De Boose <nikoms@gmail.com>
2021-01-30 17:22:52 +0100

    Second step of the refactoring to MVP: the "presenter's viewModel" is used by the view and the view use the presenter to update it. Everytime the viewModel is updated, the view is re-rendered

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -3,6 +3,8 @@ import {createTodo} from './todo.service';
 
 class TodoListPresenter {
   constructor() {
+    // Someone outside will be able to listen to modification of the viewModel
+    this.viewModelListener = (viewModel) => null;
     this.viewModel = {
       todos: [
         createTodo('Frozen yoghurt', false),
@@ -13,18 +15,31 @@ class TodoListPresenter {
       ],
     };
   }
+
+  // This is how we set the listener of the presenter
+  onViewModelChange(callback) {
+    this.viewModelListener = callback;
+  }
+  // This method will be called by the view
+  setTodoList(todos) {
+    this.viewModel.todos = todos;
+    //Calling the listener with a copy of the viewModel ("immutable" style)
+    this.viewModelListener({...this.viewModel});
+  }
 }
 
-const TodoList = ({presenter, viewModel }) => {
-  const [todos, setTodoList] = useState(viewModel.todos);
+const TodoList = ({presenter, viewModel}) => {
+  // We don't need state anymore, because the todos from the viewModel will always be updated
+  const todos = viewModel.todos;
 
   const ongoingCount = useMemo(() => todos.filter(t => !t.done).length, [todos]);
   const doneCount = useMemo(() => todos.filter(t => t.done).length, [todos]);
 
   const [clickCount, setClickCount] = useState(0);
 
-  const addEmptyTodo = () => setTodoList([createTodo('Relax! Edition will come...', false), ...todos]);
-  const toggleDone = (index) => setTodoList([...todos.slice(0, index), {
+  // Really simple refac: We call a "setTodoList" instead of the local one
+  const addEmptyTodo = () => presenter.setTodoList([createTodo('Relax! Edition will come...', false), ...todos]);
+  const toggleDone = (index) => presenter.setTodoList([...todos.slice(0, index), {
     ...todos[index],
     done: !todos[index].done,
   }, ...todos.slice(index + 1)]);
@@ -52,11 +67,19 @@ const TodoList = ({presenter, viewModel }) => {
 
 export const withMVP = (Wrapped) =>
   function WithTodoPresenter() {
+    // The purpose of this viewModel is to keep the last version of the presenter's viewModel 
+    // AND it will also help us to re-render the Component thanks to "setViewModel".
+    const [viewModel, setViewModel] = useState();
+
     const presenter = useMemo(() => {
-      return new TodoListPresenter();
+      const presenter = new TodoListPresenter();
+      // When the viewModel changes, give it to "setViewModel" to reload the component with the new value
+      presenter.onViewModelChange(setViewModel);
+      return presenter;
     }, []);
 
-    return <Wrapped presenter={presenter} viewModel={presenter.viewModel}/>;
+    // The first time, viewModel will be undefined. When "onViewModelChange" will be called, viewModel will have the last value
+    return <Wrapped presenter={presenter} viewModel={viewModel || presenter.viewModel}/>;
   };
 
 
