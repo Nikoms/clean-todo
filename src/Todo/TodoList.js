f438ab89a1bc3bb4b76de4b3df07387398a0748c
Nicolas De Boose <nikoms@gmail.com>
2021-01-30 22:26:44 +0100

    Remove comment for the next refactoring

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -3,7 +3,6 @@ import {createTodo} from './todo.service';
 
 class TodoListPresenter {
   constructor() {
-    // Someone outside will be able to listen to modification of the viewModel
     this.viewModelListener = (viewModel) => null;
     this.viewModel = {
       todos: [
@@ -16,20 +15,16 @@ class TodoListPresenter {
     };
   }
 
-  // This is how we set the listener of the presenter
   onViewModelChange(callback) {
     this.viewModelListener = callback;
   }
-  // This method will be called by the view
   setTodoList(todos) {
     this.viewModel.todos = todos;
-    //Calling the listener with a copy of the viewModel ("immutable" style)
     this.viewModelListener({...this.viewModel});
   }
 }
 
 const TodoList = ({presenter, viewModel}) => {
-  // We don't need state anymore, because the todos from the viewModel will always be updated
   const todos = viewModel.todos;
 
   const ongoingCount = useMemo(() => todos.filter(t => !t.done).length, [todos]);
@@ -37,7 +32,6 @@ const TodoList = ({presenter, viewModel}) => {
 
   const [clickCount, setClickCount] = useState(0);
 
-  // Really simple refac: We call a "setTodoList" instead of the local one
   const addEmptyTodo = () => presenter.setTodoList([createTodo('Relax! Edition will come...', false), ...todos]);
   const toggleDone = (index) => presenter.setTodoList([...todos.slice(0, index), {
     ...todos[index],
@@ -67,18 +61,14 @@ const TodoList = ({presenter, viewModel}) => {
 
 export const withMVP = (Wrapped) =>
   function WithTodoPresenter() {
-    // The purpose of this viewModel is to keep the last version of the presenter's viewModel 
-    // AND it will also help us to re-render the Component thanks to "setViewModel".
     const [viewModel, setViewModel] = useState();
 
     const presenter = useMemo(() => {
       const presenter = new TodoListPresenter();
-      // When the viewModel changes, give it to "setViewModel" to reload the component with the new value
       presenter.onViewModelChange(setViewModel);
       return presenter;
     }, []);
 
-    // The first time, viewModel will be undefined. When "onViewModelChange" will be called, viewModel will have the last value
     return <Wrapped presenter={presenter} viewModel={viewModel || presenter.viewModel}/>;
   };
 
