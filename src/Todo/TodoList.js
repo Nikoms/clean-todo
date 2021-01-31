dbe9a4c74ed9d8c52a8b054fdf1bb2c0c9212383
Nicolas De Boose <nikoms@gmail.com>
2021-01-31 14:55:23 +0100

    Remove comment for the next refactoring

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -18,19 +18,13 @@ const fetchTodosFromApi = async () => {
 const TodoList = () => {
   const [todos, setTodoList] = useState([]);
   useEffect(function callApi() {
-    //1. Imagine that we are calling an api to get todos 
     fetchTodosFromApi().then(list => setTodoList(list));
   }, []);
 
-  //2. We still have our counters that are recalculate on every render
   const ongoingCount = todos.filter(t => t.done).length;
   const doneCount = todos.filter(t => !t.done).length;
 
-  // 3. And we have a new cool feature that count the number of time we click on titles (best feature ever asked by the CEO)
   const [clickCount, setClickCount] = useState(0);
-
-  //Conclusion: 
-  // - Calling "setClickCount" will re-render the component and our "ongoingCount" and "doneCount" will be recalculate everytime... It's fine for this example, but it can be problematic if the computation is heavy and can block your main thread
 
   const addEmptyTodo = () => setTodoList([createTodo('Relax! Edition will come...', false), ...todos]);
   const markAsDone = (index) => setTodoList([...todos.slice(0, index), {
