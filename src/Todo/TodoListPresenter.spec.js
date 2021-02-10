47449c45b305cb5c128b616fff59bb39a73f61d0
Nicolas De Boose <nikoms@gmail.com>
2021-02-10 15:54:29 +0100

    Use the real viewModel to make sure the listener is called on upgrade

--- src/Todo/TodoListPresenter.spec.js
+++ src/Todo/TodoListPresenter.spec.js
@@ -9,22 +9,25 @@ jest.mock('./todo.service', () => ({
 test('it fetches todos from api', async () => {
   getTodos.mockResolvedValue([createTodo('My first todo', false), createTodo('My second todo', true)]);
   const presenter = new TodoListPresenter(getTodos);
-  
+  let viewModel = {};
+  presenter.onViewModelChange((vm) => viewModel = vm);
+
   await presenter.loadTodos();
-  
-  const todos = presenter.immutableViewModel().todos;
-  expect(todos).toHaveLength(2);
-  expect(todos.find(t => t.title === 'My first todo').done).toBe(false);
-  expect(todos.find(t => t.title === 'My second todo').done).toBe(true);
+
+  expect(viewModel.todos).toHaveLength(2);
+  expect(viewModel.todos.find(t => t.title === 'My first todo').done).toBe(false);
+  expect(viewModel.todos.find(t => t.title === 'My second todo').done).toBe(true);
 });
 
 test('a todo can be set as done', async () => {
   getTodos.mockResolvedValue([createTodo('My first todo', false)]);
   const presenter = new TodoListPresenter(getTodos);
+  let viewModel = {};
+  presenter.onViewModelChange((vm) => viewModel = vm);
   await presenter.loadTodos();
-  
-  expect(presenter.immutableViewModel().ongoingCount).toBe(1);
-  expect(presenter.immutableViewModel().doneCount).toBe(0);
+
+  expect(viewModel.ongoingCount).toBe(1);
+  expect(viewModel.doneCount).toBe(0);
 
   presenter.toggleDone(0);
 
@@ -35,26 +38,30 @@ test('a todo can be set as done', async () => {
 test('a todo can be set as ongoing', async () => {
   getTodos.mockResolvedValue([createTodo('My first todo', true)]);
   const presenter = new TodoListPresenter(getTodos);
+  let viewModel = {};
+  presenter.onViewModelChange((vm) => viewModel = vm);
   await presenter.loadTodos();
 
-  expect(presenter.immutableViewModel().ongoingCount).toBe(0);
-  expect(presenter.immutableViewModel().doneCount).toBe(1);
+  expect(viewModel.ongoingCount).toBe(0);
+  expect(viewModel.doneCount).toBe(1);
 
   presenter.toggleDone(0);
 
-  expect(presenter.immutableViewModel().ongoingCount).toBe(1);
-  expect(presenter.immutableViewModel().doneCount).toBe(0);
+  expect(viewModel.ongoingCount).toBe(1);
+  expect(viewModel.doneCount).toBe(0);
 });
 
 test('a todo can be added', async () => {
   getTodos.mockResolvedValue([]);
   const presenter = new TodoListPresenter(getTodos);
+  let viewModel = {};
+  presenter.onViewModelChange((vm) => viewModel = vm);
   await presenter.loadTodos();
-  expect(presenter.immutableViewModel().todos).toHaveLength(0);
-  
+  expect(viewModel.todos).toHaveLength(0);
+
   presenter.addEmptyTodo();
-  
-  const newTodos = presenter.immutableViewModel().todos;
-  expect(newTodos).toHaveLength(1);
-  expect(newTodos[0].done).toBe(false);
+
+  expect(viewModel.todos).toHaveLength(1);
+  expect((viewModel.todos)[0].done).toBe(false);
 });
+  
